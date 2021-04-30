2bd815e495e8d8402ec3a2f9850e55482de5593b
Taisuke Oe <oeuia.t@gmail.com>
2021-04-30 17:01:30 +0900

    minimum reproducible project about DiagrammedAssertions error

---
+++ build.sbt
@@ -0,0 +1,3 @@
+crossScalaVersions := Seq("3.0.0-RC3", "2.13.5")
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.8" % Test
---
+++ src/test/scala/DiagramTest.scala
@@ -0,0 +1,8 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.diagrams.Diagrams
+
+class DiagramTest extends AnyFunSuite with Diagrams{
+    test("this should be success"){
+        assert(1 === 1)
+    }
+}
